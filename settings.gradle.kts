85f1241418c32f7c6d07008dd328d87ffe2ba15e
logeshdinahypto <logeshdina@hypto.in>
2022-02-08 18:21:31 +0530

    Initial commit by BuilderHub

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "karthick-vinod-service-kotlin-client"
+
+include("karthick-vinod-service-models", "karthick-vinod-service-client")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
